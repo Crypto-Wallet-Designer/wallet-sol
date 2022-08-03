8122a454cc1b76d3599a2877c84695b252907674
Mohammad Rezaei <moh@eulith.com>
2022-08-03 15:00:14 -0400

    Working transfer test

--- test/MonWalletTest.ts
+++ test/MonWalletTest.ts
@@ -1,6 +1,9 @@
 import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { WALLET_ONE, WALLET_TWO, WALLET_THREE, WALLET_FOUR, WALLET_FIVE, WALLET_SIX,
+  KEY_ONE, KEY_TWO, KEY_THREE, KEY_FOUR, KEY_FIVE, KEY_SIX,
+  transferTo, sign, walletTransfer, transferToAddress } from "./Wallets";
 
 describe("MoNWalletTest", function () {
   // We define a fixture to reuse the same setup in every test.
@@ -12,19 +15,32 @@ describe("MoNWalletTest", function () {
 
     const MoNWallet = await ethers.getContractFactory("MoNWallet");
     const publicKeys = [
-        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-        "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-        "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"] // First three addresses in hardhat local node.
+        WALLET_ONE.address,
+        WALLET_TWO.address,
+        WALLET_THREE.address] // First three addresses in hardhat local node.
 
     const stw = await MoNWallet.deploy(2, publicKeys);
 
     return { stw, owner };
   }
 
-  describe("Deployment 2 of 3", function () {
-    it("Should have a valid address", async function () {
+  describe("Transfer tests", function () {
+    it("Should transfer", async function () {
       const { stw, owner } = await loadFixture(deploy2of3);
       expect(stw.address).contains("0x");
+      expect(await stw.getNonce()).to.eq(0);
+      const ONE_ETHER = ethers.utils.parseEther("1.0");
+      const TWO_ETHER = ethers.utils.parseEther("2.0");
+      const FIVE_ETHER = ethers.utils.parseEther("5.0");
+
+      await transferToAddress(stw.address,FIVE_ETHER);
+      expect(await ethers.provider.getBalance(stw.address)).to.equal(FIVE_ETHER);
+      await walletTransfer(stw, [KEY_ONE, KEY_TWO], WALLET_SIX.address, ONE_ETHER);
+      expect(await ethers.provider.getBalance(WALLET_SIX.address)).to.equal(ONE_ETHER);
+      await walletTransfer(stw, [KEY_ONE, KEY_THREE], WALLET_SIX.address, ONE_ETHER);
+      expect(await ethers.provider.getBalance(WALLET_SIX.address)).to.equal(TWO_ETHER);
+      // expect(await walletTransfer(stw, [KEY_ONE], WALLET_SIX.address, ONE_ETHER)).to.revertedWith("Not Authorized");
+
     });
   });
 
